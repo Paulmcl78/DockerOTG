2f1b04c2a29d31e4a374dfd6ace951d2572fb5cc
Paul McLaughlin <pmclaughlin@navinet.net>
2015-03-10 16:13:05 +0000

    Base

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNet.Mvc;
+using MvcSample.Web.Models;
+
+namespace MvcSample.Web
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(User());
+        }
+
+        public User User()
+        {
+            User user = new User()
+            {
+                Name = "Team 31",
+                Address = "NaviNet"
+            };
+
+            return user;
+        }
+
+	[Route("test")]
+	public IActionResult Hometest()
+	{
+	    return View(User());
+	}
+
+
+    }
+}
