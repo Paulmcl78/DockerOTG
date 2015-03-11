fa5bb8f3f2e0359fec0047684ae47e4c59bf898d
Andrew Kilgore <akilgore@navinet.net>
2015-03-11 10:53:15 +0000

    Send message to ActiveMQ

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNet.Mvc;
 using MvcSample.Web.Models;
 
+using Apache.Nms;
+using Apache.NMS.ActiveMQ.Commands;
+using Apache.NMS.Util;
+
 namespace MvcSample.Web
 {
     public class HomeController : Controller
@@ -24,7 +28,22 @@ namespace MvcSample.Web
 	[Route("test")]
 	public IActionResult Hometest()
 	{
-		
+		var connectionFactory = new NMSConnectionFactory("tcp://http://ec2-52-11-191-106.us-west-2.compute.amazonaws.com/:61616"))
+            
+		using (var connection = connectionFactory.CreateConnection())
+		{
+			connection.Start();
+
+			using (var session = connection.CreateSession())
+			{
+				using (var producer = session.CreateProducer(SessionUtil.GetQueue(session, "Login.Event.Q")))
+				{
+					var message = new ActiveMQTextMessage("Hello, World");
+
+					producer.Send(message);
+				}
+			}
+		}
 		
 		
 	    return View(User());
